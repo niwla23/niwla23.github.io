8f73a52813d97f4f131939205a7d7c7854e9d51b
niwla23 <alwin@cloudserver.click>
2022-05-29 21:43:01 +0200

    add title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'alwin-website',
+    title: 'Alwins Website',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Software, Hardware, 3D-Druck und Linux',
+      },
       {
         name: 'google-site-verification',
         content: 'kZyFgxt9LwbTUaFhYPhSFiW7S_Pmew7XT9jktc1WSZc',
