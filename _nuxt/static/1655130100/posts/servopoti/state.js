b9c6e8cb8a6b5b9437da51b701e5b99f1f37b741
niwla23 <46248939+niwla23@users.noreply.github.com>
2022-06-13 14:21:44 +0000

    Deploying to gh-pages from @ niwla23/niwla23.github.io@c86e7f479b0570128d3653c8137c952d35b18a53 🚀

---
+++ _nuxt/static/1655130100/posts/servopoti/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655130100",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fservopoti",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"139888af"}},_img:{"/_ipx/s_1280x584/servopoti/thumb.jpg":"\u002F_nuxt\u002Fimage\u002F69a27e.jpg"}}}(null));
