5de77e9fc1530865701196c859192814ce3052ff
niwla23 <46248939+niwla23@users.noreply.github.com>
2022-06-26 22:37:13 +0000

    Deploying to gh-pages from @ niwla23/niwla23.github.io@b679dfc125042baa6a944bae01c966bf5ffc1e80 🚀

--- _nuxt/static/1656241308/posts/giskil/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656241308",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fgiskil",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"549c8cca"}},_img:{"/_ipx/s_1280x584/giskil/thumb.png":"\u002F_nuxt\u002Fimage\u002F713c07.png"}}}(null));
