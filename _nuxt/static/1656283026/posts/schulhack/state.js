2cfbea5c4c36a8d422cd0d7a369f86ddc68579b8
niwla23 <46248939+niwla23@users.noreply.github.com>
2022-06-26 22:40:51 +0000

    Deploying to gh-pages from @ niwla23/niwla23.github.io@344a359f000d2eae59137f0be8639f5781000415 🚀

--- _nuxt/static/1656283026/posts/schulhack/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656283026",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fschulhack",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"adecd8ca"}},_img:{"/_ipx/s_1280x584/schulhack/thumb.png":"\u002F_nuxt\u002Fimage\u002Fe40b85.png"}}}(null));
