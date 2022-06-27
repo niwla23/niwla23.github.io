1fb4f66a65e446cd1c11e966679fb04a87b4b6b1
niwla23 <46248939+niwla23@users.noreply.github.com>
2022-06-27 10:45:43 +0000

    Deploying to gh-pages from @ niwla23/niwla23.github.io@ce3e2007eda6c3d856f566adb8732a67687bc080 🚀

---
+++ _nuxt/static/1656326737/posts/auctiongame/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656326737",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fauctiongame",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"67bd3bf5"}},_img:{"/_ipx/s_1280x584/auctiongame/thumb.png":"\u002F_nuxt\u002Fimage\u002F4f63ca.png"}}}(null));
---
+++ _nuxt/static/1656326737/posts/giskil/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656326737",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fgiskil",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"67bd3bf5"}},_img:{"/_ipx/s_1280x584/giskil/thumb.png":"\u002F_nuxt\u002Fimage\u002F713c07.png"}}}(null));
